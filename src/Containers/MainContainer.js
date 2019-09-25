e28b0a6907db6e6765511169663fa5b2011bb283
Peter Viss <peter.c.viss@biola.edu>
2019-09-24 20:18:27 -0400

    tweaks

--- src/Containers/MainContainer.js
+++ src/Containers/MainContainer.js
@@ -1,9 +1,13 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { List } from '../Components/List'
 import { Form } from '../Components/Form'
 const MainContainer = () => {
   const [inputText, setText] = useState('')
   const [toDoList, addToDo] = useState([])
+
+  //   useEffect(() => {
+  //     return setText('')
+  //   })
   return (
     <>
       <div>To Do List</div>
